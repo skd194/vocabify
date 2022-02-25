19e910de1084ac81525de7f15557c9f513858fdd
Shyam Kumar S <shyamkumarsb007@gmail.com>
2022-02-25 09:36:37 +0530

    updates

---
+++ api/Api/Api/Controllers/SampleController.cs
@@ -0,0 +1,23 @@
+﻿using Application.ServiceContracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("sample")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public SampleController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet("{id}")]
+        public  object Get(int id)
+        {
+            return new {Message = "Sample text", Id = id};
+        }
+    }
+}
